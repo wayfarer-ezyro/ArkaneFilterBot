de05d546ddd7de560bb89793aad49590733b2806
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-04-27 08:03:11 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -90,7 +90,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
     try:
         chat.kick_member(user_id)
         keyboard = []
-        reply = "User {} have been, successfully banned!".format(mention_html(member.user.id, member.user.first_name))
+        reply = "{} have been, successfully banned!".format(mention_html(member.user.id, member.user.first_name))
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
@@ -99,7 +99,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
             chat_id = update.effective_chat.id
             message = update.effective_message
             # Do not reply
-            reply = "User {} have been, successfully banned!".format(mention_html(member.user.id, member.user.first_name))
+            reply = "{} have been, successfully banned!".format(mention_html(member.user.id, member.user.first_name))
             bot.send_message(chat_id, reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
 #           message.reply_text('Banned!', quote=False)
             return log
