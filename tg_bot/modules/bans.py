4f89f48473c297f6e99c1f0c5f250dded9146540
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-04-27 07:41:50 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -89,7 +89,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
 
     try:
         chat.kick_member(user_id)
-        bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
+        bot.send_sticker(chat.id, BAN_STICKER)
         keyboard = []
         reply = "User {} have been, successfully banned!".format(mention_html(member.user.id, member.user.first_name))
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
@@ -174,7 +174,7 @@ def temp_ban(bot: Bot, update: Update, args: List[str]) -> str:
 
     try:
         chat.kick_member(user_id, until_date=bantime)
-        bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
+        bot.send_sticker(chat.id, BAN_STICKER)
         message.reply_text("Banned! User will be banned for {}.".format(time_val))
         return log
 
@@ -226,7 +226,7 @@ def kick(bot: Bot, update: Update, args: List[str]) -> str:
 
     res = chat.unban_member(user_id)  # unban on current user = kick
     if res:
-        bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
+        bot.send_sticker(chat.id, BAN_STICKER)
         message.reply_text("Kicked!")
         log = "<b>{}:</b>" \
               "\n#KICKED" \
