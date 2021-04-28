ce44c796ca25184970e077ffc9fcb072552d6d39
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-04-28 18:00:51 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -23,7 +23,7 @@ Hi {}, my name is {}! Iâ€™m a Group Management Bot, created and maintained by [ð
 
 Join my [update channel](https://t.me/EywasUpdates) & [support group](https://t.me/EywasSC), to stay in touch with my new updates, to ask your doubts about me & to report the bugs.
 
-Tap /help to get the list of commands.
+Press /help to get the list of commands.
 
 """
 
