947140489f76f001f83da377399bef27b8fc905b
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-06-13 20:27:46 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,7 +28,7 @@ Hit /help to get the list of commands.
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
+Hey there! My name is *{}*[.](https://telegra.ph/Σνα-20-06-13)
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
